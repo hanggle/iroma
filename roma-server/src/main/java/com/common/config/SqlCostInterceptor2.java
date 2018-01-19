05b079a62e3e57335a489a39514131c3cdd9c6f3
z.hang <oskyhang@qq.com>
2018-01-19 20:39:38 +0800

    init webapp

---
+++ roma-server/src/main/java/com/common/config/SqlCostInterceptor2.java
@@ -0,0 +1,31 @@
+package com.common.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Description:
+ * User: z.hang
+ * Date: 2018-01-14
+ * Time: 17:18
+ */
+public class SqlCostInterceptor2 implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}

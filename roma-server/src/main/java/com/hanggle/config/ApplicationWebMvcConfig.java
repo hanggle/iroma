6602264dd97d50926fad0bc833e896a9754fbdb9
z.hang <hanggle@qq.com>
2018-04-08 23:01:01 +0800

    添加swagger

--- roma-server/src/main/java/com/hanggle/config/ApplicationWebMvcConfig.java
+++ roma-server/src/main/java/com/hanggle/config/ApplicationWebMvcConfig.java
@@ -1,18 +1,27 @@
-package com.microhang.config;
+package com.hanggle.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
 
 /**
  * description:
  * author: Smile
  * date: 2017/4/8
  */
-//@Configuration
-//@EnableWebMvc
+@Configuration
+@EnableWebMvc
 public class ApplicationWebMvcConfig extends WebMvcConfigurerAdapter {
     private static Logger logger = LoggerFactory.getLogger(ApplicationWebMvcConfig.class);
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //将所有/static/** 访问都映射到classpath:/static/ 目录下
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 
   /*  @Override
     public void addInterceptors(InterceptorRegistry registry) {
---
+++ roma-server/src/main/java/com/hanggle/config/Swagger2.java
@@ -0,0 +1,36 @@
+package com.hanggle.config;
+
+import org.springframework.context.annotation.*;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * description: API <br/>
+ * author: zh <br/>
+ * date: 2018/4/8 <br/>
+ */
+@Configuration
+public class Swagger2 {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //.apis(RequestHandlerSelectors.basePackage("com.oskyhang.*.handler"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("IRoma RESTful APIs")
+                .description("go go go")
+                .termsOfServiceUrl("http://blog.hanggle.com/")
+                .contact(new Contact("hanggle", "http://blog.hanggle.com/", "hanggle@qq.com"))
+                .version("1.0")
+                .build();
+    }
+}

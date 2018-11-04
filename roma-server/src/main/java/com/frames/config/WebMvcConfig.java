d4d689f7da7d22fdce10ed4621445fc019c8fbde
hanggle <hanggle@qq.com>
2018-11-04 13:04:51 +0800

    添加jackson转换，返回前端时长整型转成字符串

--- roma-server/src/main/java/com/frames/config/WebMvcConfig.java
+++ roma-server/src/main/java/com/frames/config/WebMvcConfig.java
@@ -1,14 +1,17 @@
 package com.frames.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.math.BigInteger;
 import java.text.DateFormat;
 
 /**
@@ -23,24 +26,22 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder;
 
-    @Override
-    public void configureContentNegotiation(
-            ContentNegotiationConfigurer configurer) {
-        configurer.favorPathExtension(false);
-    }
-
     @Bean
-    public MappingJackson2HttpMessageConverter MappingJsonpHttpMessageConverter() {
-        log.info("WebMvcConfig init");
+    public MappingJackson2HttpMessageConverter mappingJsonpHttpMessageConverter() {
+        log.info("mappingJsonpHttpMessageConverter[]WebMvcConfig init");
         ObjectMapper mapper = jackson2ObjectMapperBuilder.build();
 
         // ObjectMapper为了保障线程安全性，里面的配置类都是一个不可变的对象
         // 所以这里的setDateFormat的内部原理其实是创建了一个新的配置类
         DateFormat dateFormat = mapper.getDateFormat();
         mapper.setDateFormat(new MyDateFormat(dateFormat));
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(BigInteger.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        simpleModule.addSerializer(String.class, ToStringSerializer.instance);
+        mapper.registerModule(simpleModule);
 
-        MappingJackson2HttpMessageConverter mappingJsonpHttpMessageConverter = new MappingJackson2HttpMessageConverter(
-                mapper);
-        return mappingJsonpHttpMessageConverter;
+        return new MappingJackson2HttpMessageConverter(mapper);
     }
 }
